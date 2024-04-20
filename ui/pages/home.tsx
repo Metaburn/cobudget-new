a6c943e40e5503e2fe121388a8f882fd3e1f97e6
Gal Bracha <galbra@gmail.com>
2024-04-20 22:34:53 +0300

    chore: updated text for home page

--- ui/pages/home.tsx
+++ ui/pages/home.tsx
@@ -10,7 +10,9 @@ const ProjectCard = ({ project }) => {
     return (
         <div
             data-testid="dream-card"
-            className="relative bg-white rounded-lg shadow-md overflow-hidden flex flex-col w-full hover:shadow-lg transition-shadow duration-75 ease-in-out"
+            className="relative bg-white rounded-lg shadow-md
+            overflow-hidden flex flex-col w-full hover:shadow-lg
+            transition-shadow duration-75 ease-in-out"
         >
             <img
                 src={project.image}
@@ -18,7 +20,7 @@ const ProjectCard = ({ project }) => {
             />
             <Label
                 className={
-                    "absolute right-0 m-2 " + "bg-app-green"
+                    `absolute right-0 m-2 ${project.labelColor}`
                 }
             >
                 {project.slug}
@@ -27,7 +29,7 @@ const ProjectCard = ({ project }) => {
                 <div className="mb-2">
                     <h3 className="text-xl font-medium mb-1 truncate">{project.title}</h3>
 
-                    <div className="text-gray-800">{project.summary}</div>
+                    <div className="text-gray-800"><span dangerouslySetInnerHTML={{__html: project.summary}} /></div>
                 </div>
                 <div>
                     <div className="flex gap-x-3 mt-1">
@@ -42,7 +44,7 @@ const ProjectCard = ({ project }) => {
 const HomePage = ({ currentUser }) => {
     return (
         <>
-        <div className="mt-28 lg:ml-28 lg:mr-28">
+        <div className="mt-28 lg:ml-28 lg:mr-28 sm:ml-8 sm:mr-8">
             <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 relative pb-20">
                 {projects.map((project) => (
                 <Link
@@ -76,16 +78,21 @@ const HomePage = ({ currentUser }) => {
 const projectCamps = {
     image: "https://static.wixstatic.com/media/a1a386_b23c1b1487cc4edc802fcb21772579d5~mv2.jpg/v1/fill/w_512,h_434,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/a1a386_b23c1b1487cc4edc802fcb21772579d5~mv2.jpg",
     title: "Camps - קמפים",
-    summary: "Looking for a camp? מחפש קמפים?",
+    summary: "המקום לקמפים לגייס חברים חדשים לחיים ולחברים למצוא בית\n" +
+        "<br/><br/>The place to recruit new friends for life and find a home",
     slug: "camps",
+    labelColor: "bg-pink",
     id: "camps"
 }
 
 const projectDreams = {
     image: "https://static.wixstatic.com/media/a1a386_0d4758c1296847faa5ead1ee3f8617c2~mv2.jpg/v1/fill/w_512,h_434,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/a1a386_0d4758c1296847faa5ead1ee3f8617c2~mv2.jpg",
     title: "Dreams - חלומות",
-    summary: "Want to build a spaceship? מחפש לבנות ספינת חלל או להצטרף לצוות. זה המקום",
+    summary: "מחפש לבנות ספינת חלל או להצטרף לצוות? זה המקום" +
+        "<br/><br/>" +
+        "Want to build a spaceship Or join a new team? This is the place!",
     slug: "dreams",
+    labelColor: "bg-app-purple",
     id: "dreams"
 }
 
